d44bd9277f107d23cf34eb0d10f86817e6f36384
Yosef Aweke <106531110+Yosiad@users.noreply.github.com>
2024-07-11 11:19:23 +0300

    initial set-up finished

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div> 
+      <h1 className="underline"> trial</h1>
+      <Button >Click me</Button>
+    </div>
+  );
+}
